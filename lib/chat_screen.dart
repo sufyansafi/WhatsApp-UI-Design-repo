2c85c06c52a39659fa2837be06aa760634bc8cf6
sufyansafi <sufyan@codingcrust.com>
2023-03-11 17:14:08 +0500

    clonee whtsapp chnging appliaction icon

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -14,128 +14,120 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            children: [
-              CircleAvatar(
-                radius: 18,
-                backgroundImage: NetworkImage(widget.image),
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              Text(widget.name)
-            ],
-          ),
-          actions: const [
-            Icon(Icons.call),
-            SizedBox(
-              width: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 18,
+              backgroundImage: NetworkImage(widget.image),
             ),
-            Icon(Icons.video_call_outlined),
             SizedBox(
-              width: 20,
+              width: 5,
             ),
+            Text(widget.name)
           ],
         ),
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: MessageHelper.itemCount,
-                      itemBuilder: (context, index) {
-                        MessageItemModel chatItem =
-                            MessageHelper.getCHatList(index);
-                        return Column(
-                          crossAxisAlignment: index % 2 == 0
-                              ? CrossAxisAlignment.end
-                              : CrossAxisAlignment.start,
-                          children: [
-                            MessageBubble(
-                              index: index,
-                              message: chatItem.mostRecentMessage,
-                              isSeen: false,
-                              isMe: false,
-                              time: chatItem.messageDate,
-                              onPress: () {},
-                              listLength: MessageHelper.itemCount,
-                              type: chatItem.type,
-                            )
-                          ],
-                        );
-                      }),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+        actions: const [
+          Icon(Icons.call),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(Icons.video_call_outlined),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: MessageHelper.itemCount,
+                itemBuilder: (context, index) {
+                  MessageItemModel chatItem =
+                      MessageHelper.getCHatList(index);
+                  return Column(
+                    crossAxisAlignment: index % 2 == 0
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.grey.shade100),
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 15),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: TextFormField(
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          color: Color(0xff9C9EB9)),
-                                      onChanged: (value) {},
-                                      decoration: InputDecoration(
-                                        contentPadding: EdgeInsets.symmetric(
-                                            vertical: 8.0, horizontal: 0.0),
-                                        hintText: 'Message...',
-                                        hintStyle: TextStyle(
-                                          color: Color(0xff8E8E93),
-                                        ),
-                                        focusedBorder: InputBorder.none,
-                                        enabledBorder: InputBorder.none,
-                                        errorBorder: InputBorder.none,
-                                        disabledBorder: InputBorder.none,
-                                      ),
-                                    ),
-                                  ),
-                                  Icon(Icons.attach_file),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Icon(Icons.camera_alt),
-                                  SizedBox(
-                                    width: 5,
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const CircleAvatar(
-                        radius: 20,
-                        child: Icon(
-                          Icons.mic,
-                          size: 19,
-                          color: Colors.white,
-                        ),
+                      MessageBubble(
+                        index: index,
+                        message: chatItem.mostRecentMessage,
+                        isSeen: false,
+                        isMe: false,
+                        time: chatItem.messageDate,
+                        onPress: () {},
+                        listLength: MessageHelper.itemCount,
+                        type: chatItem.type,
                       )
                     ],
+                  );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.grey.shade100),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                style: TextStyle(
+                                    fontSize: 16, color: Color(0xff9C9EB9)),
+                                onChanged: (value) {},
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 8.0, horizontal: 0.0),
+                                  hintText: 'Message...',
+                                  hintStyle: TextStyle(
+                                    color: Color(0xff8E8E93),
+                                  ),
+                                  focusedBorder: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                  errorBorder: InputBorder.none,
+                                  disabledBorder: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            Icon(Icons.attach_file),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(Icons.camera_alt),
+                            SizedBox(
+                              width: 5,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const CircleAvatar(
+                  radius: 20,
+                  child: Icon(
+                    Icons.mic,
+                    size: 19,
+                    color: Colors.white,
                   ),
                 )
               ],
             ),
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
@@ -165,99 +157,97 @@ class MessageBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: index % 2 == 0
-              ? CrossAxisAlignment.end
-              : CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 2,
-            ),
+      child: Column(
+        crossAxisAlignment: index % 2 == 0
+            ? CrossAxisAlignment.end
+            : CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 2,
+          ),
 
-            if (type == "msj")
-              Align(
-                alignment: index % 2 == 0
-                    ? Alignment.centerRight
-                    : Alignment.centerLeft,
-                child: InkWell(
-                  onTap: onPress,
-                  child: Material(
-                    elevation: 1,
-                    color: index % 2 == 0
-                        ? Colors.teal
-                        : Colors.black.withOpacity(0.5),
-                    borderRadius: index % 2 == 0
-                        ? BorderRadius.only(
-                            topLeft: Radius.circular(10.0),
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(10))
-                        : BorderRadius.only(
-                            topRight: Radius.circular(10.0),
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(0)),
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                      child: Text(
-                        message,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(fontSize: 12, color: Colors.white),
-                      ),
+          if (type == "msj")
+            Align(
+              alignment: index % 2 == 0
+                  ? Alignment.centerRight
+                  : Alignment.centerLeft,
+              child: InkWell(
+                onTap: onPress,
+                child: Material(
+                  elevation: 1,
+                  color: index % 2 == 0
+                      ? Colors.teal
+                      : Colors.black.withOpacity(0.5),
+                  borderRadius: index % 2 == 0
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(0),
+                          bottomRight: Radius.circular(10))
+                      : BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(0)),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    child: Text(
+                      message,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(fontSize: 12, color: Colors.white),
                     ),
                   ),
                 ),
               ),
-            if (type == "image")
-              Align(
-                alignment: index % 2 == 0
-                    ? Alignment.centerRight
-                    : Alignment.centerLeft,
-                child: InkWell(
-                  onTap: onPress,
-                  child: Material(
-                    elevation: 1,
-                    borderRadius: index % 2 == 0
-                        ? BorderRadius.only(
-                            topLeft: Radius.circular(10.0),
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(10))
-                        : BorderRadius.only(
-                            topRight: Radius.circular(10.0),
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(0)),
-                    color: index % 2 == 0
-                        ? Colors.teal
-                        : Colors.black.withOpacity(0.5),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Image(
-                          height: MediaQuery.of(context).size.height * .2,
-                          width: MediaQuery.of(context).size.width * .5,
-                          fit: BoxFit.cover,
-                          image: NetworkImage(message)),
-                    ),
+            ),
+          if (type == "image")
+            Align(
+              alignment: index % 2 == 0
+                  ? Alignment.centerRight
+                  : Alignment.centerLeft,
+              child: InkWell(
+                onTap: onPress,
+                child: Material(
+                  elevation: 1,
+                  borderRadius: index % 2 == 0
+                      ? BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(0),
+                          bottomRight: Radius.circular(10))
+                      : BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(0)),
+                  color: index % 2 == 0
+                      ? Colors.teal
+                      : Colors.black.withOpacity(0.5),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Image(
+                        height: MediaQuery.of(context).size.height * .2,
+                        width: MediaQuery.of(context).size.width * .5,
+                        fit: BoxFit.cover,
+                        image: NetworkImage(message)),
                   ),
                 ),
               ),
-
-            SizedBox(
-              height: 2,
-            ),
-            Text(
-              time.toString(),
-              maxLines: 1,
-              style: Theme.of(context).textTheme.bodyText1,
             ),
 
-            SizedBox(
-              height: 4,
-            ),
-            // Text(time.toString())
-          ],
-        ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            time.toString(),
+            maxLines: 1,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+
+          SizedBox(
+            height: 4,
+          ),
+          // Text(time.toString())
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -11,51 +11,58 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return ChangeNotifierProvider<ThemeChanger>(
         create: (_) => ThemeChanger(),
-        child: Builder(
-        builder: (BuildContext context) {
+        child: Builder(builder: (BuildContext context) {
           final themeChanger = Provider.of<ThemeChanger>(context);
-          return  MaterialApp(
+          return MaterialApp(
             title: 'Flutter Demo',
-            home:  HomeScreen(),
+            home: HomeScreen(),
             themeMode: themeChanger.themeMode,
             theme: ThemeData(
               brightness: Brightness.light,
               primarySwatch: Colors.teal,
               textTheme: const TextTheme(
-                  headline3: TextStyle(fontFamily: 'Bold', fontSize: 20.0,color: Colors.black ),
-                  headline4: TextStyle(fontFamily: 'Bold', fontSize: 18.0,color: Color(0xff25292B) ),
-                  headline5: TextStyle(fontFamily: Bold,fontSize: 16.0,color: Color(0xff25292B) ),
-                  headline6: TextStyle(fontFamily: Bold, fontSize: 14.0,color: Color(0xff25292B)),
-                  bodyText1: TextStyle(fontFamily: Regular, fontSize: 12.0, color: Color(0xff25292B)),
-                  bodyText2: TextStyle(fontFamily: Regular, fontSize: 10.0, color:Color(0xff25292B)),
+                headline3: TextStyle(
+                    fontFamily: 'Bold', fontSize: 20.0, color: Colors.black),
+                headline4: TextStyle(
+                    fontFamily: 'Bold',
+                    fontSize: 18.0,
+                    color: Color(0xff25292B)),
+                headline5: TextStyle(
+                    fontFamily: Bold, fontSize: 16.0, color: Color(0xff25292B)),
+                headline6: TextStyle(
+                    fontFamily: Bold, fontSize: 14.0, color: Color(0xff25292B)),
+                bodyText1: TextStyle(
+                    fontFamily: Regular,
+                    fontSize: 12.0,
+                    color: Color(0xff25292B)),
+                bodyText2: TextStyle(
+                    fontFamily: Regular,
+                    fontSize: 10.0,
+                    color: Color(0xff25292B)),
               ),
-                iconTheme: IconThemeData(
-                color: Colors.grey.shade600
-          ),
+              iconTheme: IconThemeData(color: Colors.grey.shade600),
             ),
-
             darkTheme: ThemeData(
-              brightness: Brightness.dark,
+                brightness: Brightness.dark,
                 textTheme: const TextTheme(
-                    headline3: TextStyle(fontFamily: 'Bold', fontSize: 20.0,color: Colors.white ),
-                    headline4: TextStyle(fontFamily: 'Bold', fontSize: 18.0,color: Colors.white ),
-                    headline5: TextStyle(fontFamily: Bold,fontSize: 16.0, color: Colors.white),
-                    headline6: TextStyle(fontFamily: Bold, fontSize: 14.0,color: Colors.white),
-                    bodyText1: TextStyle(fontFamily: Regular, fontSize: 12.0, color: Colors.white),
-                    bodyText2: TextStyle(fontFamily: Regular, fontSize: 10.0,color: Colors.white),
+                  headline3: TextStyle(
+                      fontFamily: 'Bold', fontSize: 20.0, color: Colors.white),
+                  headline4: TextStyle(
+                      fontFamily: 'Bold', fontSize: 18.0, color: Colors.white),
+                  headline5: TextStyle(
+                      fontFamily: Bold, fontSize: 16.0, color: Colors.white),
+                  headline6: TextStyle(
+                      fontFamily: Bold, fontSize: 14.0, color: Colors.white),
+                  bodyText1: TextStyle(
+                      fontFamily: Regular, fontSize: 12.0, color: Colors.white),
+                  bodyText2: TextStyle(
+                      fontFamily: Regular, fontSize: 10.0, color: Colors.white),
                 ),
-          iconTheme: IconThemeData(
-              color: Colors.grey
-          )
-            ),
+                iconTheme: IconThemeData(color: Colors.grey)),
             debugShowCheckedModeBanner: false,
           );
-
-        })
-    );
+        }));
   }
 }
-
